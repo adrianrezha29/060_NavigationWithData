39420a4e1d8a89d47aa0184ace68eb342b557184
adrianrezha29 <adrianrezha29@gmail.com>
2023-11-09 01:38:10 +0700

    finish

---
+++ app/src/main/java/com/example/act5/Data/SumberData.kt
@@ -0,0 +1,14 @@
+package com.example.act5.Data
+
+import com.example.act5.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+
+    )
+}
