fac38df9d97353da6109be44d5bb1181bb67ac49
adrianrezha29 <adrianrezha29@gmail.com>
2023-11-16 16:57:58 +0700

    add column

--- app/src/main/java/com/example/act5/HalamanForm.kt
+++ app/src/main/java/com/example/act5/HalamanForm.kt
@@ -1,5 +1,9 @@
 package com.example.act5
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -7,6 +11,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,4 +30,13 @@ fun HalamanSatu(
         mutableStateOf("")
     }
     var ListDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ){
+
+    }
 }
