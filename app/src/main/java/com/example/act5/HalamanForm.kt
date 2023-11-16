f673c419609bf6d11f9aad7f795e80ec6dd691cb
adrianrezha29 <adrianrezha29@gmail.com>
2023-11-16 16:59:57 +0700

    add spacer and button submit

--- app/src/main/java/com/example/act5/HalamanForm.kt
+++ app/src/main/java/com/example/act5/HalamanForm.kt
@@ -2,8 +2,10 @@ package com.example.act5
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -54,5 +57,9 @@ fun HalamanSatu(
         }, label = {
             Text(text = "Telpon")
         })
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(onClick = {onSubmitButtonClick(ListDataTxt)}) {
+            Text(text = stringResource(id = R.string.btn_submit))
+        }
     }
 }
