98efb8ddde38d09389aa43e0bb2b8cbcfcf39244
adrianrezha29 <adrianrezha29@gmail.com>
2023-11-16 16:49:17 +0700

    add nama,alamat,no_tlp on order ui state

--- app/src/main/java/com/example/act5/Data/OrderUIState.kt
+++ app/src/main/java/com/example/act5/Data/OrderUIState.kt
@@ -4,4 +4,8 @@ data class OrderUIState(
     val jumlah: Int =0,
     val rasa: String = "",
     val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val noTlp: String = ""
+
 )
